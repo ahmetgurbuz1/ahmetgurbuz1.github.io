68e2721aabaab0cc642fcb77ec6df49b8cc4fa74
AhmetGurbuz <grbz523@gmail.com>
2023-07-11 20:51:33 +0300

    metin2 okey event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import {useState} from "react";
 
 function App() {
+  const [allCardSet, setAllCardSet] = useState(
+    Array.from(Array(8).keys())
+      .map(l => l + 1)
+      .map(_ => {
+        return {
+          "yellow": false,
+          "blue": false,
+          "red": false
+        }
+      })
+  )
+
+  const innerBoxOnClick = (cardRow, inner) => {
+    const newRow = allCardSet[cardRow]
+    newRow[inner] = true
+    const start = allCardSet.slice(0,cardRow)
+    const end = allCardSet.slice(cardRow+1,allCardSet.length+1)
+    const newCardSet = start.concat([newRow]).concat(end)
+    setAllCardSet(newCardSet)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
       </header>
+      <main className={"App-main"}>
+        {
+          allCardSet.map((cardSet, cardRow, c) =>
+            <div className={"outer-box"}>
+              {
+                ["yellow", "blue", "red"].map((inner, cardColumn, f) =>
+                  <div
+                    className={`inner-box ${inner} ${cardSet[inner] === true ? "box-disabled" : null}`}
+                    onClick={(event) => innerBoxOnClick(cardRow, inner)}
+                  >{cardRow + 1}</div>
+                )
+              }
+            </div>
+          )
+        }
+      </main>
     </div>
   );
 }
